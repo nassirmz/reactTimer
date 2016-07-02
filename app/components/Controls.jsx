b9d399000ce45e3199c901e8b1adcc58439b0127
nassir <nassirmz@gmail.com>
2016-07-01 19:54:38 -0700

    (feat) Add static controls component

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -2,22 +2,28 @@ var React = require('react');
 
 var Controls = React.createClass({
   propTypes: {
-    countdownStatus: React.PropTypes.string.isRequired
+    countdownStatus: React.PropTypes.string.isRequired,
+    onStatusChange: React.PropTypes.func.isRequired
+  },
+  onStatusChange: function (newStatus) {
+    return () => {
+      this.props.onStatusChange(newStatus);
+    }
   },
   render: function () {
     var {countdownStatus} = this.props;
     var renderStartStopButton = () => {
       if(countdownStatus ==='started') {
-        return <button className="btn btn-secondary-outline">Pause</button>
+        return <button type="button" className="btn btn-primary-outline" onClick={this.onStatusChange('paused')}>Pause</button>
       }
       if(countdownStatus === 'paused') {
-        return <button className="btn btn-secondary-outline">Start</button>
+        return <button type="button" className="btn btn-primary-outline" onClick={this.onStatusChange('started')}>Start</button>
       }
     };
     return (
       <div className="controls">
         {renderStartStopButton()}
-        <button className="btn btn-danger-outline">Clear</button>
+        <button type="button" className="btn clear" onClick={this.onStatusChange('stopped')}>Clear</button>
       </div>
     );
   }
